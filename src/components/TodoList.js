5f06c64b931075e3a617d10396503e7aa164d4a4
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-10-29 22:19:07 +0600

    Footer is updated

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 const TodoList = () => {
     const todos = useSelector((state) => state.todos);
     return (
-        <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+        <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
             {todos.map((todo) => (
                 <Todo key={todo.id} todo={todo} />
             ))}
