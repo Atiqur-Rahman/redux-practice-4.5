66ffd026981c927b831b7887b549265e3132da35
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-10-31 00:37:01 +0600

    Header is updated

--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -14,6 +14,7 @@ const reducer = (state = initialState, action) => {
                 {
                     id: nextTodoId(state),
                     text: action.payload,
+                    completed: false,
                 },
             ];
 
